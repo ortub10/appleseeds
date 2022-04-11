a1be516997a5363d929bbc8aa38b9f49352cc7a2
ortub10 <ortub10@gmail.com>
2022-04-11 21:04:09 +0300

    first commit

--- 11.04.2022 js/8.2_object methods.js
+++ 11.04.2022 js/8.2_object methods.js
@@ -8,9 +8,9 @@ const myCountry = {
         console.log(`${this.country} has ${this.population} million people, their mother tongue is ${this.language}, they
         have ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`);
     },
-    // checkIsland(){
-    //     this['isIsland'] = this.neighbours.length===0?true:false;
-    // }
+    checkIsland(){
+        this['isIsland'] = this.neighbours.length===0?true:false;
+    }
 }
 //call the descrbie method
 myCountry.describe();
