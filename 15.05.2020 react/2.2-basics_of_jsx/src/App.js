ac048c2769ee004cfd436e1e4afa8b9d3b479441
ortub10 <ortub10@gmail.com>
2022-05-15 16:12:08 +0300

    first commit

---
+++ 15.05.2020 react/2.2-basics_of_jsx/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const data = ["hello", "world"];
+  const number1 = 5;
+  const number2 = 6;
+
+  const str = "I love React!";
+  return (
+    <div className="App">
+      <p>{data.join(" ")}</p>
+      <br />
+      <hr />
+      <p>{`${number1} + ${number2} = ${number1 + number2}`}</p>
+      <br />
+      <hr />
+      <p>{`The string's length is ${str.length}`}</p>
+    </div>
+  );
+}
+
+export default App;
