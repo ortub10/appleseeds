5ae519f6184e7c1fb950498f7179461f0ad3617d
ortub10 <ortub10@gmail.com>
2022-06-15 23:09:10 +0300

    first commit

--- 15.06.2022 node/express_with_react/backend/app.js
+++ 15.06.2022 node/express_with_react/backend/app.js
@@ -2,13 +2,12 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
-const PORT = 3050;
+const PORT = process.env.PORT || 3050;
 
 app.use(express.json());
 app.use(cors({ origin: ["http://localhost:3000"] }));
 
 const BASE_URL = "http://api.weatherstack.com/current";
-// const KEY = "a074e3d69976dd56a41e98243dd18f1c";
 const KEY = "812faae0de38e3fc47c24b3c95827204";
 
 app.get("/:city", (req, res) => {
